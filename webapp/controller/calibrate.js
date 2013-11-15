6cf65a9be1f5b62406071f3e35915d2df008ccb1
Kaustubh Bhalerao <bhalerao.1@gmail.com>
2013-11-15 17:09:31 -0600

    modified calibrate.js

--- webapp/controller/calibrate.js
+++ webapp/controller/calibrate.js
@@ -2,9 +2,9 @@ var i2c = require('./i2cbase');
 
 var fs = require('fs');
 
-var zcal = 503.4; 
+var zcal = 464.1; 
 
-var file = fs.createWriteStream('./cal_100_503_low.txt');
+var file = fs.createWriteStream('./cal_100_503_low4p.txt');
 
 var myparams = {start:50, increment:50, steps:79, range:"L"};
 
@@ -18,7 +18,7 @@ for(var j=0; j<myparams.steps; j++) {
 
 file.end();
 
-var file = fs.createWriteStream('./cal_1k_503_high.txt');
+var file = fs.createWriteStream('./cal_1k_503_high4p.txt');
 
 var myparams = {start:4000, increment:100, steps:460, range:"H"};
 
