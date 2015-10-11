8a59763cf7bc07c05a4493082c2efedd9609248d
Kaustubh Bhalerao <bhalerao@illinois.edu>
2015-10-11 14:50:12 -0500

    added R file to smooth text, and a new calibration file

---
+++ webapp/controller/smoother.R
@@ -0,0 +1,15 @@
+## Calibration smoother
+
+low <- read.csv("cal_100_264_low.txt", header = FALSE)
+high <- read.csv("cal_1k_264_high.txt", header = FALSE)
+
+dat <- rbind(low, high)
+with(dat, plot(V1,V2, log='xy'))
+sm <- with(dat, smooth.spline(V1, V2))
+with(sm, points(x,y, col="red"))
+
+with(dat, plot(V1,V3, log='xy'))
+sp <- with(dat, smooth.spline(V1,V3))
+with(sp, points(x,y, col="red"))
+
+newframe <- data.frame(f = dat$V1, m = sm$y, p= sp$y)
